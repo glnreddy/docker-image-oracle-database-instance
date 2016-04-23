b0f17024f7e672b191c09034afc9ddda0b992fad
Jorge Quilcate <quilcate.jorge@gmail.com>
2016-04-23 18:54:32 +0200

    fix started instance

---
+++ docker/startup.sh
@@ -0,0 +1,8 @@
+LISTENERS_ORA=$ORACLE_HOME/network/admin/listener.ora
+
+cp "${LISTENERS_ORA}.tmpl" "$LISTENERS_ORA" &&
+sed -i "s/%hostname%/$HOSTNAME/g" "${LISTENERS_ORA}" &&
+sed -i "s/%port%/1521/g" "${LISTENERS_ORA}" &&
+bin/lsnrctl start &&
+bin/dbstart $ORACLE_HOME &&
+tail -f startup.log
